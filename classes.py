a20281fb7e44d46850cd714806147eb8891f4049
jordangriffiths01 <jordangriffiths01@gmail.com>
2014-11-29 16:59:13 +1300

    Initial class refs for xml elements

--- classes.py
+++ classes.py
@@ -1,3 +1,7 @@
+import urllib.request
+import xml
+import xml.etree.ElementTree as ET
+
 SCHEDULE_DOMAIN = "http://xml.tab.co.nz/schedule/"
 
 class Schedule:
@@ -10,24 +14,30 @@ class Schedule:
     
     def load_xml(self):
         domain = SCHEDULE_DOMAIN
-        if date:
-            domain += date + '/'
-            if meet_no:
-                domain += meet_no
+        if self.date:
+            domain += self.date + '/'
+            if self.meet_no:
+                domain += self.meet_no
                 
         f = urllib.request.urlopen(domain)
         xml_content = f.read()
-        self.xml_schedule = ET.fromstring(self.xml_content).find('meetings')
+        self.xml_schedule = ET.fromstring(xml_content).find('meetings')
+    
+    def update(self):
+        self.load_xml()
+        self.load_meetings()
     
     def load_meetings(self):
         self.meetings = []
         for xml_meeting in self.xml_schedule.findall('meeting'):
             meeting = Meeting(xml_meeting)
-            self.meetings.append(meeting)
+            self.meetings.append(meeting)       
+    
 
 class Meeting:
     
     def __init__(self, xml_meeting):
+        
         self.xml_meeting = xml_meeting
         self.code = xml_meeting.find('code').text
         self.country = xml_meeting.find('country').text
@@ -52,7 +62,6 @@ class Race:
     def __init__(self, xml_race):
         self.xml_race = xml_race
     
-        self.race_class = xml_race.find('class').text
         self.length = xml_race.find('length').text
         self.name = xml_race.find('name').text
         
@@ -68,56 +77,42 @@ class Race:
         
         self.load_entries()
         
-    def update_xml(self, xml_race):
-        self.xml_race = xml_race
-        
-    def refresh(self):
-        self.race_class = self.xml_race.find('class').text
-        self.length = self.xml_race.find('length').text
-        self.name = self.xml_race.find('name').text
-        
-        norm_time = self.xml_race.find('norm_time').text
-        self.date = norm_time.split()[0]
-        self.time = norm_time.split()[1]
-        
-        self.number = int(xml_race.find('number').text)
-        self.stake = xml_race.find('stake').text
-        self.status = xml_race.find('status').text
-        self.track_conditions = xml_race.find('track').text
-        self.weather = xml_race.find('weather').text
-        
-        self.refresh_entries()
-        
-        
     def load_entries(self):
         self.entries = []
         for xml_entry in self.xml_race.find('entries').findall('entry'):
             entry = Entry(xml_entry)
-            self.entries.append(entry)   
+            self.entries.append(entry)      
     
-        
-        
-        
         
 class Entry:
     
     def __init__(self, xml_entry):
         self.xml_entry = xml_entry
         
-        self.barrier = int(xml_entry.find('barrier').text)
-        self.jockey = xml_entry.find('jockey').text
-        self.name = xml_entry.find('name').text
-        self.number = int(xml_entry.find('number').text)
-        self.scratched = xml_entry.find('scratched').text == '1'
+        self.barrier = self.get_attrib('barrier')
+        self.jockey = self.get_attrib('jockey')
+        self.name = self.get_attrib('name')
+        self.number = self.get_attrib('number')
+        self.scratched = self.get_attrib('scratched')
         
-    def update_xml(self, xml_entry):
-        self.xml_entry = xml_entry
     
-    def refresh(self):
-        self.barrier = int(self.xml_entry.find('barrier').text)
-        self.jockey = self.xml_entry.find('jockey').text
-        self.name = self.xml_entry.find('name').text
-        self.number = int(self.xml_entry.find('number').text)
-        self.scratched = self.xml_entry.find('scratched').text == '1'        
+    def get_attrib(self, attrib, int_reqd=False):
+        try:
+            if int_reqd:
+                return int(self.xml_entry.find(attrib).text)
+            else:
+                return self.xml_entry.find(attrib).text
+        except:
+            return None
+                
         
         
+if __name__ == '__main__':
+    schedule = Schedule('2014-11-14', '3')
+    for meeting in schedule.meetings:
+        print('MEETING:', meeting.number)
+        for race in meeting.races:
+            print('\tRACE:', race.number, race.name)
+            for entry in race.entries:
+                print('\t\t', entry.barrier, entry.name, entry.jockey)
+                
